6f5de8f0c5f29102b945e4aaefc0f6968cdf4b42
Diego C. Silva <diegocaetano444@outlook.com>
2023-03-12 02:35:40 -0300

    Alterção

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (<>
-    <code className="text-white">
-      Hello Word!
+    <h1 className={inter.className}>E aí! Me explica esse código?</h1>
+
+    <code className="text-red-400">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore sequi, suscipit voluptatem rerum beatae error, mollitia similique repudiandae vel qui veritatis velit, optio repellendus explicabo veniam. Voluptas rerum ducimus tempora.  
     </code>
     </>)
 }
