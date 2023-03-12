60e370323fd5f43b1f1bd59fa212f97bb24ce834
Diego C. Silva <diegocaetano444@outlook.com>
2023-03-12 12:38:08 -0300

    Configured base layout; installed openai

--- src/pages/components/box-code.tsx
+++ src/pages/components/box-code.tsx
@@ -1,26 +1,19 @@
+import React, { ReactNode } from "react";
+
 type BoxCode = {
     className?: string,
-    code: string,
-    showLineNumbers: boolean
+    children: ReactNode,
 }
 
-const BoxCode = ({ className='', code, showLineNumbers = false }: BoxCode) => {
-
+const BoxCode = ({
+    className='',
+    children,
+}: BoxCode) => {
     return (
-        <pre className={`${className} h-[420px] w-full sm:w-1/2 lg:max-w-5xl  rounded sm:rounded-sm md:rounded-md border border-solid border-white/50`}>
-            {/* {showLineNumbers && (
-                <span className="absolute top-0 left-0 text-gray-400 text-sm px-4 py-2 select-none">
-                    {highlightedCode.split("\n").map((_, index) => (
-                        <span key={index}>{index + 1}</span>
-                    ))}
-                </span>
-            )} */}
-            <code
-                className={`hljs`}
-            ></code>
+        <pre className={`${className} h-60 sm:h-[420px] w-full sm:w-1/2 lg:max-w-5xl rounded sm:rounded-sm md:rounded-md border border-solid border-white/50 relative p-2.5`}>
+            {children}
         </pre>
     )
 }
-
 
 export default BoxCode;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,32 +3,47 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Layout from './components/layout'
 import BoxCode from './components/box-code'
+import { useRef, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [inputCode, setInputCode] = useState(''); // @Type code in language <nome da linguagem>
+  const [explanation, setExplanation] = useState('');
+
+  const heandlerCodeReading = () => {
+    setExplanation("Deu certo... possivel explicação");
+  }
+
+  const handleTextAreaChange = ({ target }: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = target;
+    setInputCode(value);
+  }
+
   return (
     <Layout>
-      <h1 className={`${inter.className} text-5xl font-bold text-center sm:mt-20 sm:mb-0 my-10 mx-5`}>E aí! Me explica esse código?</h1>
+      <h1 className={`${inter.className} text-5xl font-bold text-center sm:mt-20 sm:mb-0 mt-10 mx-5`}>E aí! Me explica esse código?</h1>
       
       <button
-        onClick={() => {""}}
+        onClick={heandlerCodeReading}
         type="button"
-        className={"font-medium rounded-lg text-base px-5 py-2.5 mt-6 bg-white/90 text-black border-white/50 hover:bg-white/90"}
-      >Explicar</button>
-
+        className={"font-medium rounded-lg text-base px-5 py-2.5 my-6 bg-white/90 text-black border-white/50 hover:bg-white/90"}
+      >Decifra Esse Código!</button>
 
       <div className={"flex justify-center flex-wrap sm:flex-nowrap gap-3 h-auto w-full p-2 sm:p-5 sm:pt-10"}>
         <BoxCode
           className={"bg-black/50 backdrop-blur-sm"}
-          showLineNumbers={true}
-          code={""}
-        />
+        >
+          <textarea
+            className={"w-full h-full border-none bg-transparent resize-none outline-none text-base font-mono"}
+            onChange={handleTextAreaChange}
+            value={inputCode}
+          ></textarea>
+          
+        </BoxCode>
         <BoxCode
-          className={"bg-white/80 backdrop-blur-sm text-black"}
-          showLineNumbers={true}
-          code={""}
-        />
+          className={"bg-white/80 backdrop-blur-sm text-black overflow-auto py-2"}
+        >{explanation}</BoxCode>
       </div>
     </Layout>
   )
